f01c716d5e52e8b01a76a1176a97543688813b0f
EffBureau <francis.bureau2@gmail.com>
2024-11-20 17:36:00 -0500

    Rework isBlocking()

--- src/Solvers.java
+++ src/Solvers.java
@@ -1,22 +1,15 @@
 public class Solvers {
-	public static boolean isBlocking(Mark[][] board, Move move) {
-        // Look in all 8 directions for wedge pattern
-        int[][] directions = {{0,1}, {1,0}, {1,1}, {1,-1}, {0,-1}, {-1,0}, {-1,-1}, {-1,1}};
-        
-        for (int[] dir : directions) {
-            int c = move.getCol() + dir[0];
-            int r = move.getRow() + dir[1];
-            
+	public static boolean isBlocking(Mark[][] board, Move move, int[] direction) {
+        // Look in all 8 directions for wedge pattern        
+            int c = move.getCol() + direction[0];
+            int r = move.getRow() + direction[1];        
             if (Board.isInbound(c,r) ) {
-                // Check for stretch two pattern
-                if (board[c][r] == move.getColor().getOpponent() &&
-					(Shapes.isTria(board, c, r, move.getColor().getOpponent()) ||
-					Shapes.isTesseraWithEmptyEnd(board, c, r, move.getColor().getOpponent()))
-				) {
-                    return true;
+                // Check if the next cell is the opponent's cell
+                if(board[c][r] == move.getColor().getOpponent()) {
+                    return Shapes.isConnected(board, c, r, move.getColor().getOpponent(), 4, direction) ||
+                        Shapes.isConnected(board, c, r, move.getColor().getOpponent(), 3, direction);                        
                 }
-            }
-        }
+            }        
 
         return false;
     }
