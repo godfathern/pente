21feefc564b0499559495b85122f8267aec3f011
EffBureau <francis.bureau2@gmail.com>
2024-11-20 17:36:53 -0500

    Add super method overrides and score to Move

--- src/Move.java
+++ src/Move.java
@@ -27,12 +27,44 @@ public class Move {
         return color;
     }
 
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int evaluate) {
+        score = evaluate;
+    }
+
     public boolean isWithinNSquares(Move move, int n) {
         return Math.abs(col  - move.getCol()) <= n && Math.abs(row - move.getRow()) <= n;
     }
 
+    @Override
     public String toString(){
         return Character.toString((char) (col + 'A')) + (row + 1);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+
+        if (obj == this) {
+            return true;
+        }
+
+        if (!(obj instanceof Move)) {
+            return false;
+        }
+
+        Move move = (Move) obj;
+        return move.getCol() == col && move.getRow() == row;
+    }
+
+    @Override
+    public int compareTo(Move o) {
+        return o.getScore() - score;
     }
 }
 
